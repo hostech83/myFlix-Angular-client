80059fc0b1ac2f275c6a6ab03cecf9bce3ccdfb0
hostech83 <166859198+hostech83@users.noreply.github.com>
2025-03-02 04:16:56 -0500

    add movieDetails routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 export const routes: Routes = [
+  { path: 'movies/:id', component: MovieDetailsComponent },
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieListComponent },
   { path: 'profile', component: UserProfileComponent },
   { path: 'login', component: UserLoginFormComponent },
   { path: 'registration', component: UserRegistrationFormComponent },
-  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+  { path: '', redirectTo: 'movies', pathMatch: 'prefix' },
 ];
